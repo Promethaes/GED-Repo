6734f667ba6a349eb63ddd603c6619bf4f80fc99
Anthony <anth19800@hotmail.com>
2020-09-30 21:09:15 -0400

    [Add] Base Stuff

---
+++ Assets/Scripts/LevelEditorManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEditorManager : MonoBehaviour
+{
+
+    public List<GameObject> SpawnButtons;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggleButtonMenu();
+    }
+
+
+    bool toggle = false;
+    public void toggleButtonMenu()
+    {
+        for(int i = 0; i < SpawnButtons.Count; i++)
+            SpawnButtons[i].SetActive(toggle);
+
+        toggle = !toggle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SpawnObject()
+    {
+        var temp = GameObject.Instantiate(prefab);
+        temp.transform.position = new Vector3(0.0f, 10.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
