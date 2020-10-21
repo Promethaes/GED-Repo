d081972a824874fe3c3fa731d45caa6f196f17b1
Carter Menary <carter.menary@gmail.com>
2020-10-20 23:29:46 -0400

    observer attempt

---
+++ Assets/Scripts/Subject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Subject : MonoBehaviour
+{
+
+    private List<Observer> m_observers = new List<Observer>();
+
+    private string m_state;
+
+    public string getState()
+    {
+        return m_state;
+    }
+
+    public void setState(string state)
+    {
+        m_state = state;
+        notifyObservers();
+    }
+
+    public void attach(Observer observer)
+    {
+        m_observers.Add(observer);
+    }
+
+    public void notifyObservers()
+    {
+        //Cycles through and updates all observers
+        for (int i = 0; i < m_observers.Count; i++)
+        {
+            m_observers[i].ObserverUpdate();
+        }
+    }
+
+}
